25e2a3bc2a88b9e4855f8cab1969b0725e08ae95
Tom Wilson <tom@jackhq.com>
2016-03-06 07:49:09 -0500

    remove cors

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var jwt = require('express-jwt')
-var cors = require('cors')
 var PouchDB = require('pouchdb')
 var MemPouchDB = PouchDB.defaults({db: require('memdown')})
 
@@ -8,7 +7,6 @@ var remoteDb = process.env.COUCHDB || 'http://admin:admin@localhost:5984/w3foo'
 var localDb = process.env.LOCALDB || 'example'
 
 var app = express()
-app.use(cors())
 app.use(jwt({secret: new Buffer(process.env.JWT_SECRET, 'base64') }))
 app.use('/', require('express-pouchdb')(MemPouchDB))
 var local = MemPouchDB(localDb)
