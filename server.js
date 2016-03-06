60eb01924ec28596dba1aedc6774ff0c09da83a1
Tom Wilson <tom@jackhq.com>
2016-03-06 07:33:36 -0500

    add cors support

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var jwt = require('express-jwt')
+var cors = require('cors')
 var PouchDB = require('pouchdb')
 var MemPouchDB = PouchDB.defaults({db: require('memdown')})
 
@@ -7,6 +8,7 @@ var remoteDb = process.env.COUCHDB || 'http://admin:admin@localhost:5984/w3foo'
 var localDb = process.env.LOCALDB || 'example'
 
 var app = express()
+app.use(cors())
 app.use(jwt({secret: new Buffer(process.env.JWT_SECRET, 'base64') }))
 app.use('/', require('express-pouchdb')(MemPouchDB))
 var local = MemPouchDB(localDb)
